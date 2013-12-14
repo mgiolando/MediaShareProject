4009a7b67d7dde3000013a799d7d84bbaa3253fc
Mark Giolando <mark-robin.giolando@students.olin.edu>
2013-12-14 11:38:30 -0500

    updates

--- install_things.py
+++ install_things.py
@@ -5,7 +5,11 @@ def install_things():
     os.system('sudo apt-get install libgnome2-0')
     os.system('sudo pip install pymongo')
     os.system('sudo apt-get install mongodb')
+    os.system('sudo add-apt-repository ppa:gstreamer-developers/ppa')
+    os.system('sudo apt-get update')
+    os.system('sudo apt-get install gstreamer1.0*')
 #    os.system()
+
 
 
 
